7a49744452e3ed56a6e11ab4e1b6ff2e9182eccc
testwork1609-boop <test.work1609@gmail.com>
2026-07-16 13:29:08 +0200

    ccc

--- src/SecureDesktop/Database/Repositories/EventLogRepository.cs
+++ src/SecureDesktop/Database/Repositories/EventLogRepository.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json;
 using SecureDesktop.Models;
 
 namespace SecureDesktop.Database.Repositories
@@ -18,7 +22,26 @@ namespace SecureDesktop.Database.Repositories
             entry.Id = data.NextEventId++;
             entry.Timestamp = DateTime.Now;
             data.EventLogs.Add(entry);
+
+            // 🔍 Diagnostyka przed Save()
+            int beforeSave = data.Users.Count;
             _db.Save();
+
+            // 🔍 Diagnostyka po Save() – sprawdź, czy użytkownicy nie zginęli
+            string json = File.ReadAllText(_db.GetDatabasePath());
+            var checkData = JsonConvert.DeserializeObject<DatabaseData>(json);
+            int afterSave = checkData?.Users?.Count ?? 0;
+            string userList = string.Join(", ", checkData?.Users?.Select(u => u.IdentificationNumber) ?? new List<string>());
+
+            MessageBox.Show(
+                $"EventLog Create: {entry.OperationName}\n" +
+                $"Użytkownicy przed zapisem: {beforeSave}\n" +
+                $"Użytkownicy po zapisie: {afterSave}\n" +
+                $"Lista: {userList}\n\n" +
+                $"Ścieżka: {_db.GetDatabasePath()}",
+                "Diagnostyka EventLog",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
         }
     }
 }
